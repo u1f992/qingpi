96d4e9423e164a0835a7d0e952f64648a9c2c5c0
Koutaro Mukai <mukai.k1011k@outlook.jp>
2023-08-02 16:47:53 +0900

    Update main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -20,6 +20,7 @@ button_t btn_start;
 // button_t btn_rclick;
 button_t btn_home;
 button_t btn_power;
+button_t btn_wifi;
 button_t *btn_all[14] = {
     &btn_y,
     &btn_b,
@@ -31,10 +32,10 @@ button_t *btn_all[14] = {
     NULL,
     &btn_select,
     &btn_start,
-    NULL,
-    NULL,
+    &btn_power,
+    &btn_wifi,
     &btn_home,
-    &btn_power};
+    NULL};
 
 button_t hat_up;
 button_t hat_right;
@@ -78,11 +79,12 @@ void setup()
     button_init(&btn_start, 16);
     button_init(&btn_home, 17);
     button_init(&btn_power, 18);
+    button_init(&btn_wifi, 19);
 
-    button_init(&hat_up, 19);
-    button_init(&hat_right, 20);
-    button_init(&hat_down, 21);
-    button_init(&hat_left, 22);
+    button_init(&hat_up, 20);
+    button_init(&hat_right, 21);
+    button_init(&hat_down, 22);
+    button_init(&hat_left, 26);
     hat_init(&hat, &hat_up, &hat_right, &hat_down, &hat_left);
 
     pinMode(LED_BUILTIN, OUTPUT);
@@ -95,21 +97,21 @@ uint8_t data[11];
 size_t len = sizeof(data) / sizeof(data[0]);
 size_t ptr = 0;
 #define SERIAL_TIMEOUT 100
-int c = 0;
+int cnt = 0;
 
 void loop()
 {
     if (Serial.available() == 0)
     {
-        c++;
-        if (c == SERIAL_TIMEOUT)
+        cnt++;
+        if (cnt == SERIAL_TIMEOUT)
         {
-            c = 0;
+            cnt = 0;
             ptr = 0;
         }
         return;
     }
-    c = 0;
+    cnt = 0;
 
     data[ptr] = Serial.read();
     if (ptr == 0 && data[0] != 0xAB)
@@ -123,48 +125,12 @@ void loop()
     }
     ptr = 0;
 
-    /**
-     * @code {.unparsed}
-     * | Button (NXMC)   | Mask                  |
-     * | --------------- | --------------------- |
-     * | Y               | 0b0000'0000'0000'0001 |
-     * | B               | 0b0000'0000'0000'0010 |
-     * | A               | 0b0000'0000'0000'0100 |
-     * | X               | 0b0000'0000'0000'1000 |
-     * | L               | 0b0000'0000'0001'0000 |
-     * | R               | 0b0000'0000'0010'0000 |
-     * | (ZL)            | 0b0000'0000'0100'0000 |
-     * | (ZR)            | 0b0000'0000'1000'0000 |
-     * | Select (-)      | 0b0000'0001'0000'0000 |
-     * | Start (+)       | 0b0000'0010'0000'0000 |
-     * | (L Click)       | 0b0000'0100'0000'0000 |
-     * | (R Click)       | 0b0000'1000'0000'0000 |
-     * | Home            | 0b0001'0000'0000'0000 |
-     * | Power (Capture) | 0b0010'0000'0000'0000 |
-     * @endcode
-     */
     uint16_t btns = data[1] | data[2] << 8;
-    /**
-     * @code {.unparsed}
-     * | Hat | Value |
-     * | --- | ----- |
-     * | ↑   | 0     |
-     * | ↗   | 1     |
-     * | →   | 2     |
-     * | ↘   | 3     |
-     * | ↓   | 4     |
-     * | ↙   | 5     |
-     * | ←   | 6     |
-     * | ↖   | 7     |
-     * | x   | 8     |
-     * @endcode
-     */
     uint8_t dir = data[3];
     uint8_t lx = data[4];
     uint8_t ly = data[5];
     // uint8_t rx = data[6];
     // uint8_t ry = data[7];
-    // In NXMC these are used for keyboard input, but used for touchscreen here.
     uint8_t tx = data[8] | data[9] << 8;
     uint16_t ty = data[10];
     Serial.printf("btns:%u,dir:%u,lx:%u,ly:%u,tx:%u,ty:%u", btns, dir, lx, ly, tx, ty);
