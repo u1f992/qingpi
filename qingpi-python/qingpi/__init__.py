0b5cf2bd23deaa821f957fd2bb5a0d35de7cc688
Koutaro Mukai <mukai.k1011k@outlook.jp>
2023-12-27 01:08:58 +0900

    Expose init function

--- qingpi-python/qingpi/__init__.py
+++ qingpi-python/qingpi/__init__.py
@@ -2,3 +2,4 @@ from .qingpi import Button, Y, B, A, X, L, R, SELECT, START, POWER, WIRELESS, HO
 from .qingpi import Hat, UP, UPRIGHT, RIGHT, DOWNRIGHT, DOWN, DOWNLEFT, LEFT, UPLEFT
 from .qingpi import SlidePad
 from .qingpi import TouchScreen
+from .qingpi import init
